f4d348799b8fd94a32713114c1da84d0eeaf7f33
Victoria Babalola <vbabalola@appdev.yearup.org>
2025-06-10 08:40:29 -0500

    Data display

--- NorthwindTraders/src/main/java/com/pluralsight/NorthwindTrader.java
+++ NorthwindTraders/src/main/java/com/pluralsight/NorthwindTrader.java
@@ -1,5 +1,4 @@
 package com.pluralsight;
-import com.mysql.cj.jdbc.exceptions.SQLError;
 
 import java.sql.*;
 
@@ -9,26 +8,51 @@ public class NorthwindTrader {
         String username = "root";
         String password = "yearup";
 
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
         try {
+
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            try (
-                    Connection connection = DriverManager.getConnection(url, username, password);
-                    PreparedStatement statement = connection.prepareStatement("SELECT ProductName FROM Products");
-                    ResultSet resultSet = statement.executeQuery();
-            ) {
-                System.out.println("Products in the Northwind DB:");
-                System.out.println("--------------------------------");
+            connection = DriverManager.getConnection(url, username, password);
 
-                while (resultSet.next()) {
-                    String name = resultSet.getString("ProductName");
-                    System.out.println("- " + name);
-                }
+            statement = connection.prepareStatement(
+                    "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products"
+            );
+            resultSet = statement.executeQuery();
+
+            System.out.println("Products in the Northwind DB:");
+            System.out.println("--------------------------------");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ProductID");
+                String name = resultSet.getString("ProductName");
+                double price = resultSet.getDouble("UnitPrice");
+                int stock = resultSet.getInt("UnitsInStock");
+
+                System.out.println("Product Id: " + id);
+                System.out.println("Name      : " + name);
+                System.out.println("Price     : " + price);
+                System.out.println("Stock     : " + stock);
+                System.out.println("----------------------");
             }
 
         } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("There was an error!");
+            System.out.println(" There was an error:");
             e.printStackTrace();
+
+        } finally {
+
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException ex) {
+                System.out.println(" Error closing resources:");
+                ex.printStackTrace();
+            }
         }
     }
 }
